74cff7182fe585c75338991c037a5e9ac3fdf825
hiroki0 <key0note@gmail.com>
2014-10-19 00:05:22 +0900

    implement login to zaim

--- app/src/main/java/hm/orz/key0note/zaimcalendar/LoginActivity.java
+++ app/src/main/java/hm/orz/key0note/zaimcalendar/LoginActivity.java
@@ -1,106 +1,88 @@
 package hm.orz.key0note.zaimcalendar;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.AsyncTask;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-import oauth.signpost.OAuth;
-import oauth.signpost.OAuthConsumer;
-import oauth.signpost.OAuthProvider;
-import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
-import oauth.signpost.commonshttp.CommonsHttpOAuthProvider;
-import oauth.signpost.exception.OAuthCommunicationException;
-import oauth.signpost.exception.OAuthExpectationFailedException;
-import oauth.signpost.exception.OAuthMessageSignerException;
-import oauth.signpost.exception.OAuthNotAuthorizedException;
 
 public class LoginActivity extends ActionBarActivity {
 
-    private static final String CONSUMER_KEY = "ee23cafabb4b76471aa40a773849d72b4fda2aef";
-    private static final String CONSUMER_SECRET = "e60b89aa662adffabdf4bafa802c81165df946cf";
-    private static final String REQUEST_TOKEN_URL = "https://api.zaim.net/v2/auth/request";
-    private static final String AUTHORIZE_URL = "https://auth.zaim.net/users/auth";
-    private static final String ACCESS_TOKEN_URL = "https://api.zaim.net/v2/auth/access";
-    private static final String CALLBACK = "myapp://callback";
+    private static final String CALLBACK = "http://zaimcalendar_callback/";
+    private static final String LOGIN_COMPLETE_URL = "https://auth.zaim.net/users/auth";
 
-    private OAuthConsumer mConsumer;
-    private OAuthProvider mProvider;
-
-    private class OAuthRequestAsyncTask extends AsyncTask<Void, Void, Void> {
-        @Override
-        protected Void doInBackground(Void...arg0) {
-            //OAuth認証
-            try {
-                mConsumer = new CommonsHttpOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
-                mProvider = new CommonsHttpOAuthProvider(REQUEST_TOKEN_URL, ACCESS_TOKEN_URL, AUTHORIZE_URL);
-
-                String authUrl = mProvider.retrieveRequestToken(mConsumer, CALLBACK);
-
-                // ブラウザに認証ページを開かせる
-                LoginActivity.this.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(authUrl)));
-            } catch (OAuthMessageSignerException e) {
-                e.printStackTrace();
-            } catch (OAuthNotAuthorizedException e) {
-                e.printStackTrace();
-            } catch (OAuthExpectationFailedException e) {
-                e.printStackTrace();
-            } catch (OAuthCommunicationException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-    }
-
-    private class OAuthAccessAsyncTask extends AsyncTask<Uri, Void, Void> {
-        @Override
-        protected Void doInBackground(Uri...uris) {
-            Uri uri = uris[0];
-            final String oauthVerifier = uri.getQueryParameter(OAuth.OAUTH_VERIFIER);
-            try {
-                // AccessToken取得
-                mProvider.retrieveAccessToken(mConsumer, oauthVerifier);
-                Log.d(this.getClass().getName(), "ACCESS_TOKEN : " + mConsumer.getToken());
-                Log.d(this.getClass().getName(), "ACCESS_TOKEN_SECRET : " + mConsumer.getTokenSecret());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-    }
+    private ZaimOAuthClient mZaimOAuthClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        setContentView(R.layout.activity_login);
 
-        //
-        OAuthRequestAsyncTask asyncTask = new OAuthRequestAsyncTask();
-        asyncTask.execute();
+        final WebView webView = (WebView) findViewById(R.id.web_view);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                Log.v(this.getClass().getName(), "onPageFinished url = " + url);
+
+                if (url.equals(LOGIN_COMPLETE_URL)) {
+                    webView.loadUrl("javascript:window.alert(document.getElementsByTagName(\'code\')[0].innerHTML);");
+                }
+            }
+        });
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                Log.d(this.getClass().getName(), "url = " + url);
+                Log.d(this.getClass().getName(), "message = " + message);
+
+                // argument message is OAuth Verifier
+                mZaimOAuthClient.access(message, new ZaimOAuthClient.AccessCallback() {
+                    @Override
+                    public void onComplete() {
+                        Log.d(this.getClass().getName(), "ACCESS_TOKEN : " + mZaimOAuthClient.getToken());
+                        Log.d(this.getClass().getName(), "ACCESS_TOKEN_SECRET : " + mZaimOAuthClient.getTokenSecret());
+
+                        // back to main activity
+                        finish();
+                    }
+                });
+
+                return true;
+            }
+        });
+
+        mZaimOAuthClient = new ZaimOAuthClient();
+        mZaimOAuthClient.request(CALLBACK, new ZaimOAuthClient.RequestCallback() {
+            @Override
+            public void onComplete(String authUrl) {
+                Log.d(this.getClass().getName(), "authUrl = " + authUrl);
+                webView.loadUrl(authUrl);
+            }
+        });
     }
 
-
     @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-
-        Uri uri = intent.getData();
-        // ブラウザ認証からのコールバック
-        if (uri != null && uri.toString().startsWith(CALLBACK)) {
-            // アクセストークン取得およびリクエスト処理
-            OAuthAccessAsyncTask asyncTask = new OAuthAccessAsyncTask();
-            asyncTask.execute();
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        WebView webView = (WebView) findViewById(R.id.web_view);
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack();
+            return true;
         }
+
+        return super.onKeyDown(keyCode, event);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
+        getMenuInflater().inflate(R.menu.login, menu);
         return true;
     }
 
@@ -118,4 +100,5 @@ public class LoginActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
