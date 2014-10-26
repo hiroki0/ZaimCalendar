6899a8f8ca86988c5fa1934b8110a7dc6bb66d1c
hiroki0 <key0note@gmail.com>
2014-10-27 00:07:55 +0900

    implement to get used money list and and show by calendar view

--- app/src/main/java/hm/orz/key0note/zaimcalendar/MainActivity.java
+++ app/src/main/java/hm/orz/key0note/zaimcalendar/MainActivity.java
@@ -3,19 +3,36 @@ package hm.orz.key0note.zaimcalendar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class MainActivity extends ActionBarActivity {
+
+    private final String TAG = MainActivity.class.getSimpleName();
+    private final int REQUEST_CODE_LOGIN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ZaimCalendarView calendarView = (ZaimCalendarView) findViewById(R.id.calender);
+        calendarView.set(2014, 10 - 1);
+
         Intent intent = new Intent();
         intent.setClassName("hm.orz.key0note.zaimcalendar", "hm.orz.key0note.zaimcalendar.LoginActivity");
         MainActivity.this.startActivity(intent);
+        startActivityForResult(intent, REQUEST_CODE_LOGIN);
     }
 
     @Override
@@ -43,5 +60,62 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if (requestCode == REQUEST_CODE_LOGIN) {
+            ZaimOAuthClient authClient = new ZaimOAuthClient(
+                    SharedPreferenceUtils.getAccessToken(getApplicationContext()),
+                    SharedPreferenceUtils.getAccessTokenSecret(getApplicationContext())
+            );
+            ZaimClient client = new ZaimClient(authClient);
+            client.userVerify(new ZaimClient.RequestCallback() {
+                @Override
+                public void onComplete(String response) {
+                    Log.v("zaim api user verify", "response = " + response);
+                }
+            });
+            client.getMoneyList(new ZaimClient.RequestCallback() {
+                @Override
+                public void onComplete(String response) {
+                    Log.v("zaim api get money list", "response = " + response);
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        JSONArray array = jsonObject.getJSONArray("money");
+                        for (int i = 0; i < array.length(); i++) {
+                            JSONObject data = array.getJSONObject(i);
+                            String dateString = data.getString("date");
+                            String amountString = data.getString("amount");
+
+                            DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+                            format.parse(dateString);
+                            Calendar calendar = format.getCalendar();
+
+                            if (calendar.get(Calendar.MONTH) == 9) {
+                                ZaimCalendarView calendarView = (ZaimCalendarView) findViewById(R.id.calender);
+                                calendarView.setDataOfDay(
+                                        calendar.get(Calendar.MONTH) + 1,
+                                        calendar.get(Calendar.DAY_OF_MONTH),
+                                        Integer.parseInt(amountString));
+                            }
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+        }
+        switch (requestCode) {
+            case REQUEST_CODE_LOGIN:
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/hm/orz/key0note/zaimcalendar/ZaimClient.java
@@ -0,0 +1,90 @@
+package hm.orz.key0note.zaimcalendar;
+
+import android.os.AsyncTask;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class ZaimClient {
+
+    private static final String ZAIM_DOMAIN = "https://api.zaim.net";
+
+    public interface RequestCallback {
+        public void onComplete(String response);
+    }
+
+    private class RequsetAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        private HttpGet mHttpGet;
+        private String mResponse;
+        private RequestCallback mCallback;
+
+        RequsetAsyncTask(HttpGet httpGet, RequestCallback callback) {
+            mHttpGet = httpGet;
+            mCallback = callback;
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            HttpClient httpClient = new DefaultHttpClient();
+
+            try {
+                mResponse = httpClient.execute(mHttpGet, new ResponseHandler<String>() {
+                    @Override
+                    public String handleResponse(HttpResponse httpResponse) throws ClientProtocolException, IOException {
+                        // HttpStatus.SC_OK (HTTP200)
+                        if (httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                            return EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
+                        }
+                        return null;
+                    }
+                });
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void v) {
+            mCallback.onComplete(mResponse);
+        }
+    }
+
+    private ZaimOAuthClient mZaimOAuthClient;
+
+    ZaimClient(ZaimOAuthClient zaimOAuthClient) {
+        mZaimOAuthClient = zaimOAuthClient;
+    }
+
+    public void userVerify(RequestCallback callback) {
+        String urlString = ZAIM_DOMAIN + "/v2/home/user/verify";
+
+        HttpGet httpGet = new HttpGet(urlString);
+        mZaimOAuthClient.sign(httpGet);
+
+        RequsetAsyncTask asyncTask = new RequsetAsyncTask(httpGet, callback);
+        asyncTask.execute();
+    }
+
+    public void getMoneyList(RequestCallback callback) {
+        String urlString = ZAIM_DOMAIN + "/v2/home/money";
+
+        HttpGet httpGet = new HttpGet(urlString);
+        mZaimOAuthClient.sign(httpGet);
+
+        RequsetAsyncTask asyncTask = new RequsetAsyncTask(httpGet, callback);
+        asyncTask.execute();
+    }
+
+}
